b15798e156e62746d917069da82b7ba7f3182de8
bhoopendraa97 <bhoopendraa77@gmail.com>
2024-11-26 19:40:08 +0530

    add_event

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+
+/*const App=()=>{
+  const display=()=>{
+  alert("Event example!!")
+  }
+ return(
+<>
+<h1>Welcome</h1>
+<button onClick={display}>click here!</button>
+</>
+ )
+
+}
+export default App; */
+
+
+/*const App=()=>{
+  const myname=(nm)=>{
+  alert(`my name is :${nm}`)
+  }
+ return(
+<>
+<h1>Welcome</h1>
+<button onClick={()=>{myname("Ayushi !")}}>click here!</button>
+</>
+ )
+}
+export default App; */
+
+ /*   const App=()=>{
+       const display=(nm,b)=>{
+      //     console.log(a);
+            alert(` name : ${nm} Event: ${b.type}`)
+         }
+         return(
+                  <>
+            <h1>Welcome</h1>
+           <button onClick={(b)=>{display("Bhoopendra",b)}}>click here</button>
+            </>
+         )
+        }
+     export default App; */
+
+ /*    const App=()=>{
+
+         const display=(e)=>{
+           alert("This is Event: "+ e.type);
+      
+      
+           }
+         return(
+           <>
+          
+            <button onClick={display}>click here</button>
+             </>
+           )
+        }
+      
+       export default App; */
+
+
+ /*   const App=()=>{
+      const myval=(e)=>{
+       let nm=e.target.name;
+       let val=e.target.value;
+       alert("Name: "+ nm + "value: "+val);
+      }
+    return(
+      <>
+      <h1>Welcome</h1>
+      <button name="danish" value="xyz" onClick={myval}>click here</button>
+   
+        </>
+
+       )
+       }
+
+     export default App; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
